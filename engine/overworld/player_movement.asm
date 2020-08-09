f079b4762228cdf40e33fc887c5d07a652c35a39
jianmingyong <jianming1993@gmail.com>
2020-08-10 02:16:51 +0800

    Fix: No bump noise if standing on tile $3E.

--- engine/overworld/player_movement.asm
+++ engine/overworld/player_movement.asm
@@ -399,8 +399,8 @@ DoPlayerMovement::
 ; making bumps silent.
 
 	ld a, [wWalkingDirection]
-	; cp STANDING
-	; jr z, .not_warp
+	cp STANDING
+	jr z, .not_warp
 	ld e, a
 	ld d, 0
 	ld hl, .EdgeWarps
@@ -413,8 +413,8 @@ DoPlayerMovement::
 	ld [wWalkingIntoEdgeWarp], a
 	ld a, [wWalkingDirection]
 	; This is in the wrong place.
-	cp STANDING
-	jr z, .not_warp
+	; cp STANDING
+	; jr z, .not_warp
 
 	ld e, a
 	ld a, [wPlayerDirection]
