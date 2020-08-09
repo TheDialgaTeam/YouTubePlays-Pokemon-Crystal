a495d24ae216055bd69a1f6fc1f3f14bd26607dd
jianmingyong <jianming1993@gmail.com>
2020-08-10 02:04:23 +0800

    Fix: Magikarp in Lake of Rage are shorter, not longer.

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -6194,10 +6194,10 @@ LoadEnemyMon:
 ; smaller than 4'0" may be caught by the filter, a lot more than intended.
 	ld a, [wMapGroup]
 	cp GROUP_LAKE_OF_RAGE
-	jr z, .Happiness
+	jr nz, .Happiness
 	ld a, [wMapNumber]
 	cp MAP_LAKE_OF_RAGE
-	jr z, .Happiness
+	jr nz, .Happiness
 ; 40% chance of not flooring
 	call Random
 	cp 39 percent + 1
